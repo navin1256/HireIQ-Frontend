acfee5235da5cbc5a44f62035fbacccd90a4af2d
Sahani-vipul <vipulsahani7600@gmail.com>
2026-05-08 13:02:52 +0530

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default config;
